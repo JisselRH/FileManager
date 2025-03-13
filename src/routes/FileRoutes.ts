ac07b857aeeb432f37e7a9b8451769c9a33c2d06
Jissel Rodriguez <jissel1707@gmail.com>
2025-03-13 09:11:19 -0400

    Ajustes url

--- src/routes/FileRoutes.ts
+++ src/routes/FileRoutes.ts
@@ -1,11 +1,13 @@
-import { Router } from 'express';
-import multer from 'multer';
-import { handleFileUpload } from '@src/services/FileService';
+import { Router } from "express";
+import multer from "multer";
+import { handleFileUpload } from "@src/services/FileService";
 
 const fileRouter = Router();
 
-const upload = multer();
-fileRouter.post('/upload', upload.single('file'), handleFileUpload);
+const upload = multer({
+  limits: { fileSize: 30 * 1024 * 1024 }, // 30MB
+});
+
+fileRouter.post("/upload", upload.single("file"), handleFileUpload);
 
 export default fileRouter;
-
